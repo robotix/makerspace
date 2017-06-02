0d59f4b926b1cf06801fa6dac77d0ae1fc253641
vidit98 <vidit.goel9816@gmail.com>
2017-06-02 23:35:36 +0530

    Fix issue of  user get access only to his docs

--- db/migrate/20170523142521_create_docs.rb
+++ db/migrate/20170523142521_create_docs.rb
@@ -1,6 +1,11 @@
 class CreateDocs < ActiveRecord::Migration[5.0]
   def change
+  	create_table :users do |t|
+  	  t.string :email
+  	  t.timestamps
+  	 end 
     create_table :docs do |t|
+      t.belong_to :user , index: true
       t.string :title
       t.text :content
 
