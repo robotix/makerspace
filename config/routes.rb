58e316e3e3a990ef2d2d29c6543aca93a5939486
vidit98 <vidit.goel9816@gmail.com>
2017-06-13 00:58:09 +0530

    Add facility to give link to google docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  resources :docs
+  resources :docs do
+    member do
+      get 'write'
+    end
+    member do
+      get 'ggldoc'
+    end	
+  end
 end
