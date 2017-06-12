fde8ee29d9d8b74bac997faf0f812ee47fe58da9
vidit98 <vidit.goel9816@gmail.com>
2017-06-13 01:03:09 +0530

    Modify some views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524140114) do
+ActiveRecord::Schema.define(version: 20170612114006) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170524140114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "gdoc"
+    t.boolean  "approved"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170524140114) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
